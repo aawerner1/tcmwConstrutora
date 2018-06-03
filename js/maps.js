f6111c025e084df8b04b9c42fa56e0939e2bf6d6
Amanda Werner <amanda.werner@hotmail.com.br>
2018-06-02 21:38:37 -0300

    Ajustes de conteúdos responsivos

--- js/maps.js
+++ js/maps.js
@@ -1,7 +1,7 @@
 function initMap() {
      var uluru = {lat: -26.3044, lng: -48.8464};
      var map = new google.maps.Map(document.getElementById('map'), {
-       zoom: 13,
+       zoom: 15,
        center: uluru
      });
      var marker = new google.maps.Marker({
